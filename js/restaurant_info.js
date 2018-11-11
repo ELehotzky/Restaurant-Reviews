999895292e2dd1d79a5c50e3d0b3f52b802d34fb
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-11 13:45:18 -0600

    map markers now working

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZWxlaG90emt5IiwiYSI6ImNqb2NkemxsZDBxOTYzcnFsMnBzazNrNGIifQ.PPgAqQhVfeZTvBXd0Y9FFA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
